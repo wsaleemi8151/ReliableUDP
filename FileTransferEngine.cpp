131262e4b906d04cf8e303db94db240941ea169b
wsaleemi <waqaralisaleemi@gmail.com>
2022-01-26 02:58:28 +0500

    Following classes added File Transfer Engine FlowControlEngine

---
+++ FileTransferEngine.cpp
@@ -0,0 +1,11 @@
+/*
+* FILE : FileTranferEngine.cpp
+* PROJECT : SENG2040-22W-Sec1-Network Application Development - Assignment # 1
+* PROGRAMMER : Gursharan Singh - Waqar Ali Saleemi
+* FIRST VERSION : 2022-01-25
+* DESCRIPTION :
+* The functions in this file are used to handle file transfer 
+*	for example, if the mode is server or client and task is sending or receiving file
+*/
+
+#include "FileTransferEngine.h"
---
+++ FlowControlEngine.cpp
@@ -0,0 +1,11 @@
+/*
+* FILE : FlowControlEngine.cpp
+* PROJECT : SENG2040-22W-Sec1-Network Application Development - Assignment # 1
+* PROGRAMMER : Gursharan Singh - Waqar Ali Saleemi
+* FIRST VERSION : 2022-01-25
+* DESCRIPTION :
+* The functions in this file are used to control the flow of data transmission and 
+	decide if the transfer mode is good or bad based on time it took to send and receive packets
+*/
+
+#include "FlowControlEngine.h"
