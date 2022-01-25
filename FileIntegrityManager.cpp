9fe9fc519caf5bd46dbcdf32a53aa7b4dcef8e41
wsaleemi <waqaralisaleemi@gmail.com>
2022-01-26 03:12:50 +0500

    Class for FileIntegrityManager.cpp added

---
+++ FileIntegrityManager.cpp
@@ -0,0 +1,11 @@
+/*
+* FILE : FileIntegrityManager.cpp
+* PROJECT : SENG2040-22W-Sec1-Network Application Development - Assignment # 1
+* PROGRAMMER : Gursharan Singh - Waqar Ali Saleemi
+* FIRST VERSION : 2022-01-25
+* DESCRIPTION :
+* The functions in this file are used to check and verify the file transfer integrity
+*/
+
+
+#include "FileIntegrityManager.h"
