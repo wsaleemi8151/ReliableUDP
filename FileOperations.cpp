e91cba6c9a414848c3fb7e5902c64e1a9c6237e5
wsaleemi <waqaralisaleemi@gmail.com>
2022-01-26 03:05:10 +0500

    File operations class added

---
+++ FileOperations.cpp
@@ -0,0 +1,11 @@
+/*
+* FILE : FileOperations.cpp
+* PROJECT : SENG2040-22W-Sec1-Network Application Development - Assignment # 1
+* PROGRAMMER : Gursharan Singh - Waqar Ali Saleemi
+* FIRST VERSION : 2022-01-25
+* DESCRIPTION :
+* The functions in this file are used to handle reading/writing files to/from the file system
+*/
+
+
+#include "FileOperations.h"
