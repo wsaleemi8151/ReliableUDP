7aee1dba6a42b214313fb69416c89c96e24772c5
Gursharan Singh <gursharansinghsandhu352@gmail.com>
2022-02-01 18:44:43 -0500

    Added Protocol header processing functions

--- FileIntegrityManager.cpp
+++ FileIntegrityManager.cpp
@@ -9,3 +9,88 @@
 
 
 #include "FileIntegrityManager.h"
+
+#include<string.h>
+#include<stdlib.h>
+#pragma warning (disable:4996)
+
+
+int AddHeader(char* fileName, char* contentType, char* contentSize, char* fileDigest, char* data)
+{
+	char temp[5000] = " ";
+	char delimiter = '#';
+
+	strcat(temp, fileName);
+	strncat(temp, &delimiter, 1);
+	strcat(temp, contentType);
+	strncat(temp, &delimiter, 1);
+	strcat(temp, contentSize);
+	strncat(temp, &delimiter, 1);
+	strcat(temp, fileDigest);
+	strncat(temp, &delimiter, 1);
+	strcat(temp, data);
+
+
+	strcpy(data, temp);
+
+	return 0;
+}
+
+int ExtractHeader(char* fileName, char* contentType, char* contentSize, char* fileDigest, char* data)
+{
+	strcpy(fileName, "");
+	strcpy(contentType, "");
+	strcpy(contentSize, "");
+	strcpy(fileDigest, "");
+	strcpy(data, "");
+
+	char temp[5000] = " ";
+	char delimiter = '#';
+
+	char c = 'a';
+	int i = 1;
+	int j = 1;
+	while (c != '\0')
+	{
+		c = data[i];
+
+		if (j != 5)
+		{
+			if (c == delimiter)
+			{
+				j++;
+				i++;
+				continue;
+			}
+		}
+
+		if (j == 1)
+		{
+			strncat(fileName, &c, 1);
+		}
+		else if (j == 2)
+		{
+			strncat(contentType, &c, 1);
+		}
+		else if (j == 3)
+		{
+			strncat(contentSize, &c, 1);
+		}
+		else if (j == 4)
+		{
+			strncat(fileDigest, &c, 1);
+		}
+		else if (j == 5)
+		{
+			strncat(data, &c, 1);
+		}
+
+		i++;
+	}
+
+	return 0;
+}
+
+
+
+
