6d6fdfaada11814cd5c45f99c229b86232bcad1e
Gursharan Singh <gursharansinghsandhu352@gmail.com>
2022-02-10 12:13:04 -0500

    Added MD5 functionality

--- FlowControlEngine.cpp
+++ FlowControlEngine.cpp
@@ -9,3 +9,37 @@
 */
 
 #include "FlowControlEngine.h"
+
+
+#include <iostream> //for std::cout
+#include <string.h> //for std::string
+#include <fstream>
+#include "MD5.h"
+
+using std::cout; using std::endl;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    //Start opening your file
+    ifstream inBigArrayfile;
+    inBigArrayfile.open("Data.dat", std::ios::binary | std::ios::in);
+
+    //Find length of file
+    inBigArrayfile.seekg(0, std::ios::end);
+    long Length = inBigArrayfile.tellg();
+    inBigArrayfile.seekg(0, std::ios::beg);
+
+    //read in the data from your file
+    char* InFileData = new char[Length];
+    inBigArrayfile.read(InFileData, Length);
+
+    //Calculate MD5 hash
+    std::string Temp = md5(InFileData, Length);
+    cout << Temp.c_str() << endl;
+
+    //Clean up
+    delete[] InFileData;
+
+    return 0;
+}
